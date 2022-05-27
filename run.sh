65c829ba1560774109836a71dea06ca39a516bbd
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2022-05-27 14:22:48 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,8 +16,9 @@ sudo docker run hello-world
 echo 'run docker ps'
 sudo docker ps
 echo 'Download and install hashicorp packer'
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt-get update && sudo apt-get install packer -y
 echo 'Verify'
 packer version
