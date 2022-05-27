0b478e3cc9d335139ed8e85cb230fb264da17925
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2022-05-27 11:34:37 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,17 +6,17 @@ sudo apt install curl -y
 echo 'add docker key'
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo 'add docker repository'
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 echo 'run apt update'
-apt update -y
+sudo apt update -y
 echo 'run apt install docker-ce'
-apt install docker-ce -y
+sudo apt install docker-ce -y
 echo 'docker hello-world'
-docker run hello-world
+sudo docker run hello-world
 echo 'run docker ps'
-docker ps
+sudo docker ps
 echo 'Download and install hashicorp packer'
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install packer
 echo 'Verify'
