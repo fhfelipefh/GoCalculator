dcb561769be464fa071c4a504588425ece500ee4
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2022-05-27 17:17:02 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 sudo docker run -p 8081:8081 fhfelipefh/calculator:latest -d &
+# set to run this command in system startup
+sudo chmod +x run.sh
+sudo cp run.sh /etc/init.d/
+sudo chmod +x /etc/init.d/run.sh
+sudo update-rc.d run.sh defaults
+sudo service run.sh start
+
+
