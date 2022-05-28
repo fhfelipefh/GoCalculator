5fecf9df9a357b09e8a659005fa4df9fe7d2395f
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2022-05-28 19:13:27 -0300

    update run and startup

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@ echo 'run docker container'
 sudo docker run -p 8081:8081 fhfelipefh/calculator:latest -d &
 echo 'docker run done'
 sudo docker ps
+echo 'Show services running in startup'
+docker update –restart=always fhfelipefh/calculator:latest
--- startup.sh
+++ startup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 sudo chmod +x run.sh
-sudo cp run.sh /etc/init.d/
+echo "copy run.sh to /etc/init.d"
+sudo cp run.sh /etc/init.d/run.sh
 sudo chmod +x /etc/init.d/run.sh
+echo "add run.sh to rc.local"
 sudo update-rc.d run.sh defaults
+echo "run.sh is installed"
 echo 'start service'
 sudo service run.sh start
 echo 'service status'
