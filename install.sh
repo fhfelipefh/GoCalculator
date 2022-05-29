9bcd3f1008d1120fa7987a51e292f80971f72d14
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2022-05-29 18:50:03 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,9 @@ echo 'add docker repository'
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 echo 'run apt update'
 sudo apt update -y
-echo 'run apt install docker-ce'
+echo 'try run apt install docker-ce'
 if ! [ -x "$(command -v docker)" ]; then
+  echo 'docker is not installed'
   sudo apt install docker-ce -y
 fi
 echo 'docker hello-world'
@@ -23,11 +24,13 @@ sudo docker ps
 echo 'Download and install hashicorp packer'
 if [ ! -f /usr/local/bin/packer ]; 
 then
+  echo 'packer is not installed'
      curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
      sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
      sudo apt-get update && sudo apt-get install packer -y
      echo 'Verify'
      packer version
 fi
+sudo apt autoremove -y
 echo 'Done'
 
